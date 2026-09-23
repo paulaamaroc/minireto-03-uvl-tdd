ba8814559d013f3eef6a00f7394f70f2a8b83e81
paulaamaroc <pauamacal@alum.us.es>
2026-09-23 13:38:36 +0200

    Ciclo 3: añadir test_five_features_is_tiny() y test_six_features_is_small()

--- tests/test_size.py
+++ tests/test_size.py
@@ -9,3 +9,9 @@ def test_one_feature_is_tiny():
 def test_zero_features_is_invalid():
     with pytest.raises(ValueError):
         classify_model_size(0)
+
+def test_five_features_is_tiny():
+    assert classify_model_size(5) == "tiny"
+
+def test_six_features_is_small():
+    assert classify_model_size(6) == "small"
