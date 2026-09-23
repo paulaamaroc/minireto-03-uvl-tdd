0bc9712ad8fd91e2e065ae09d0fa05f2378749ff
miguelraja <migrajolm@alum.us.es>
2026-09-23 13:33:56 +0200

    Ciclo 2: añadir test_zer_features_is_invalid

--- tests/test_size.py
+++ tests/test_size.py
@@ -5,3 +5,7 @@ from catalog import classify_model_size
 
 def test_one_feature_is_tiny():
     assert classify_model_size(1) == "tiny"
+
+def test_zero_features_is_invalid():
+    with pytest.raises(ValueError):
+        classify_model_size(0)
